e4cad0a4c793b63ce16f27b6334cc1b33545dafd
Steven Whitlow <54036907+stevenwhitlow@users.noreply.github.com>
2021-12-17 16:25:15 -0500

    initial site

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,25 @@
+var store = [{
+        "title": "Latent Dirichlet Allocation",
+        "excerpt":"Text as data Two primary issues: Converting text data into something we can apply statistical methods to Dealing with the high dimensonality inherent in text data In the literature, a textual dataset is known as a corpus. Each corpus is composed of \\(D\\) documents: \\(d \\in {1,...,D}\\). Each document is...","categories": ["projects"],
+        "tags": [],
+        "url": "/projects/latent-dirichlet-allocation/",
+        "teaser": null
+      },{
+        "title": "Foo Bar Identity",
+        "excerpt":"Pictures of Goats section West Seattle Blog dingbat newspaper rubber cement Google+ newsroom cafe news.me rubber cement, Ushahidi Kindle Single syndicated Instagram HuffPo community mthomps, Mozilla iPhone app should isn’t a business model curmudgeon Snarkmarket Tim Carmody production of innocence. Fuego tweets community DocumentCloud metered model Gardening &amp; War section...","categories": [],
+        "tags": [],
+        "url": "/projects/foo-bar-website/",
+        "teaser": "/assets/images/foo-bar-identity-th.jpg"
+      },{
+        "title": "Self-selection of workers across U.S. states",
+        "excerpt":"See the full paper associated with this project here. See the code on GitHub. In this project I examine patterns of immigration in the US and the resulting effect on the locational choices of workers who are born in the US, implementing a standard specification to study the impact of...","categories": ["projects"],
+        "tags": [],
+        "url": "/projects/self-selection/",
+        "teaser": null
+      },{
+        "title": "Fuzzy classification of occupational tasks",
+        "excerpt":"See the full paper associated with this project here. See the code on GitHub. To what degree are accumulated skills are transferable across different jobs? Labour market datasets typically include information on the occupations worked by individuals. The Dictionary of Occupational Titles (DOT) and its successor O*NET are two datasets...","categories": ["projects"],
+        "tags": [],
+        "url": "/projects/fuzzy-classification-occupations/",
+        "teaser": null
+      }]
