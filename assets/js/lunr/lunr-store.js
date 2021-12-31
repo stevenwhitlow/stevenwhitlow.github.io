e553c11a30b112ec47cef2f03b36a2946cc67222
Steven Whitlow <54036907+stevenwhitlow@users.noreply.github.com>
2021-12-31 00:41:24 -0500

    rename page

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -5,21 +5,15 @@ var store = [{
         "url": "/blog/latent-dirichlet-allocation/",
         "teaser": null
       },{
-        "title": "Foo Bar Identity",
-        "excerpt":"Pictures of Goats section West Seattle Blog dingbat newspaper rubber cement Google+ newsroom cafe news.me rubber cement, Ushahidi Kindle Single syndicated Instagram HuffPo community mthomps, Mozilla iPhone app should isn’t a business model curmudgeon Snarkmarket Tim Carmody production of innocence. Fuego tweets community DocumentCloud metered model Gardening &amp; War section...","categories": [],
-        "tags": [],
-        "url": "/projects/foo-bar-website/",
-        "teaser": "/assets/images/foo-bar-identity-th.jpg"
-      },{
         "title": "Self-selection of workers across U.S. states",
         "excerpt":"See the full paper associated with this project here. See the code on GitHub. In this project I examine patterns of immigration in the US and the resulting effect on the locational choices of workers who are born in the US, implementing a standard specification to study the impact of...","categories": ["projects"],
         "tags": [],
         "url": "/projects/self-selection/",
         "teaser": null
       },{
-        "title": "Fuzzy classification of occupational tasks",
+        "title": "Soft classification of occupational tasks",
         "excerpt":"See the full paper associated with this project here. See the code on GitHub. To what degree are accumulated skills are transferable across different jobs? Labour market datasets typically include information on the occupations worked by individuals. The Dictionary of Occupational Titles (DOT) and its successor O*NET are two datasets...","categories": ["projects"],
         "tags": [],
-        "url": "/projects/fuzzy-classification-occupations/",
+        "url": "/projects/soft-classification-occupations/",
         "teaser": null
       }]
